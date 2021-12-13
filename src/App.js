31c734acf7686f20c279946e748083400b563b8b
Snegarj <rjsnegaa@gmail.com>
2021-12-13 13:17:04 +0530

    3 files modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
-import Weather from './component/Weather'; 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Weather from './component/Weather';
 
-
-
-
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-
-  < Weather />
+      <Weather />
     </div>
   );
 }
